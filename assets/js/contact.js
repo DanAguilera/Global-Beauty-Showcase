c678a9de80d96fac8b5f730138764b1f1b8cb1a2
Corbin Cutter <Corbin@Lisas-iMac.local>
2022-10-10 21:16:24 -0400

    Final push 10/10/22

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,25 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-  // Add a click event on each of them
-  $navbarBurgers.forEach( el => {
-    el.addEventListener('click', () => {
-
-      // Get the target from the "data-target" attribute
-      const target = el.dataset.target;
-      const $target = document.getElementById(target);
-
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      el.classList.toggle('is-active');
-      $target.classList.toggle('is-active');
-
-    });
-  });
-
-});
-
 var counter = 1;
 
 setInterval(function(){
@@ -29,25 +7,3 @@ setInterval(function(){
     counter = 1;
   }
 }, 5000);
-
-var firstNameInput = document.querySelector("#fname");
-var lastNameInput = document.querySelector("#lname");
-var emailInput = document.querySelector("#email");
-var contactReasonInput = document.querySelector("#reason-for-contact");
-var submitButton = document.querySelector("#submit-button");
-
-submitButton.addEventListener("click", function(event) {
-  event.preventDefault();
-  
-  // create user object from submission
-  var user = {
-    firstName: firstNameInput.value.trim(),
-    lastName: lastNameInput.value.trim(),
-    email: emailInput.value.trim(),
-    contactReason: contactReasonInput.value.trim()
-  };
-
-  // set new submission to local storage 
-  localStorage.setItem("user", JSON.stringify(user));
-  console.log(localStorage);
-});
